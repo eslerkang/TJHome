2d574a79217d7abdfb126ae69bb006fce050d0f2
eslerkang <nexus3131@naver.com>
2021-09-26 15:40:50 +0900

    finish making icons

--- src/home.js
+++ src/home.js
@@ -2,11 +2,9 @@ function visibility(e) {
     value = e.text.toLowerCase();
     [...document.getElementsByClassName("articles")].map(item => {
         item.style.display="none";
-        return 0;
     });
     [...document.getElementsByClassName("menu__button")].map(item => {
         item.style.textDecoration="none";
-        return 0;
     })
     document.getElementById(value).style.display="flex";
     e.style.textDecoration="underline";
